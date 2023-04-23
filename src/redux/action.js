bc91e2282bfd065e3e6ebdd4c24c17e3b4954eed
Chaitanya Tyagi <chaitanyatyagi1540@gmail.com>
2023-04-23 13:15:11 +0530

    backend deployed && readMe left

--- src/redux/action.js
+++ src/redux/action.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const fetchProduct = (number) => {
 
     return async function (dispatch) {
-        const response = await axios.get(`http://localhost:1900/data/get-data/${number}`)
+        const response = await axios.get(`https://mobilicis-backend.onrender.com/data/get-data/${number}`)
         console.log(response.data.data)
         dispatch({
             type: "FETCH_PRODUCT",
